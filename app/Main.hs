ecb5449f525a76bcd4d23888ab3bb5cf16e46e5f
GOLDER303 <szymonstepien@interia.pl>
2023-07-21 14:48:19 +0200

    Read input and template files

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
-module Main where
+import Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.Text.IO as TIO
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  inputJsonFile <- BS.readFile "input.json"
+  inputTemplateFile <- TIO.readFile "template.txt"
+  print inputTemplateFile
+
+  case decode inputJsonFile :: Maybe Object of
+    Nothing -> putStrLn "Error parsing JSON"
+    Just decodedJson -> print decodedJson
