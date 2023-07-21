8b6ee3eebac5bc7063d3fb5f5afba4305a217cec
GOLDER303 <szymonstepien@interia.pl>
2023-07-21 14:52:54 +0200

    Add a function that populates the template

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Data.Aeson
+import Data.Aeson.Key (toText)
+import Data.Aeson.KeyMap (toList)
 import qualified Data.ByteString.Lazy.Char8 as BS
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
@@ -11,7 +15,27 @@ main = do
 
   case decode inputJsonFile :: Maybe Object of
     Nothing -> putStrLn "Error parsing JSON"
-    Just decodedJson -> print decodedJson
+    Just decodedJson -> TIO.writeFile "out.txt" $ populateTemplate inputTemplateFile decodedJson
+
+populateTemplate :: T.Text -> Object -> T.Text
+populateTemplate inputText keyValuePairs = populateTemplateHelper inputText $ toList keyValuePairs
+  where
+    populateTemplateHelper :: T.Text -> [(Key, Value)] -> T.Text
+    populateTemplateHelper inputText [] = inputText
+    populateTemplateHelper inputText ((key, value) : xs) = populateTemplateHelper (replaceWordInString (prepareKey key) (valueToText value) inputText) xs
 
 replaceWordInString :: T.Text -> T.Text -> T.Text -> T.Text
 replaceWordInString oldWord newWord = T.intercalate newWord . T.splitOn oldWord
+
+prepareKey :: Key -> T.Text
+prepareKey key = "{{" <> toText key <> "}}"
+
+valueToText :: Value -> T.Text
+valueToText (String foo) = foo
+valueToText (Number foo) = T.pack $ show foo
+valueToText (Bool foo) = boolToText foo
+valueToText _ = T.empty
+
+boolToText :: Bool -> T.Text
+boolToText True = "True"
+boolToText False = "False"
