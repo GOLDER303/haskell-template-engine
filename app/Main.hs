1b808b5c099ef495711e71e3f334ef89e46dc39b
GOLDER303 <szymonstepien@interia.pl>
2023-07-21 14:51:07 +0200

    Add a function that replaces all occurrences of a word with another in a text

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
 main :: IO ()
@@ -11,3 +12,6 @@ main = do
   case decode inputJsonFile :: Maybe Object of
     Nothing -> putStrLn "Error parsing JSON"
     Just decodedJson -> print decodedJson
+
+replaceWordInString :: T.Text -> T.Text -> T.Text -> T.Text
+replaceWordInString oldWord newWord = T.intercalate newWord . T.splitOn oldWord
